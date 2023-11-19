4f09e4cc94e0aa0d96d66786c9b3392ad52c714b
Amakoye <charlesamakoye@gmail.com>
2023-11-19 23:19:16 +0300

    create and get listings

--- listing/router.py
+++ listing/router.py
@@ -2,12 +2,12 @@ class ListingRouter:
     route_app_labels = {'listing'}
 
     def db_for_read(self, model, **hints):
-        if model._meta.app.label in self.route_app_labels:
+        if model._meta.app_label in self.route_app_labels:
             return 'listings'
         return None
 
     def db_for_write(self, model, **hints):
-        if model._meta.app.label in self.route_app_labels:
+        if model._meta.app_label in self.route_app_labels:
             return 'listings'
         return None
 
