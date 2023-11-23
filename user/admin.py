ebac9762477232f5198a4776b1c393d8a63dd665
Amakoye <charlesamakoye@gmail.com>
2023-11-23 23:40:10 +0300

    update admin panel

--- user/admin.py
+++ user/admin.py
@@ -1,3 +1,37 @@
+from typing import Any
 from django.contrib import admin
+from django.contrib.auth import get_user_model
+from listing.extras import delete_realtors_listing_data
 
+User = get_user_model()
 # Register your models here.
+
+
+class UserAdmin(admin.ModelAdmin):
+    using = 'users'
+
+    list_display = ('id', 'name', 'email', )
+    list_display_links = ('id', 'name', 'email',)
+   # list_filter = ('realtor',)
+    search_fields = ('name', 'email',)
+    list_per_page = 25
+
+    def save_model(self, request, obj, form, change):
+        obj.save(using=self.using)
+
+    def delete_model(self, request, obj):
+        email = obj.email
+        delete_realtors_listing_data(email)
+        obj.delete(using=self.using)
+
+    def get_queryset(self, request):
+        return super().get_queryset(request).using(self.using)
+
+    def formfield_for_foreignkey(self, db_field, request, **kwargs):
+        return super().formfield_for_foreignkey(db_field, request, using=self.using, **kwargs)
+
+    def formfield_for_manytomany(self, db_field, request, **kwargs):
+        return super().formfield_for_manytomany(db_field, request, using=self.using, **kwargs)
+
+
+admin.site.register(User, UserAdmin)
